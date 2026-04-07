27de18ee925bee2cb529135ee7414c2c5753ef5f
Mohamed Hamed <mohamed.hamed@gmail.com>
2026-04-07 11:42:51 +0200

    [IMP] field type

--- addons/sbs_custom_sale_order/models/sale_order.py
+++ addons/sbs_custom_sale_order/models/sale_order.py
@@ -3,6 +3,6 @@ from odoo import api,fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    buyer_order_no = fields.Text(string="Buyer's Order No")
+    buyer_order_no = fields.Char(string="Buyer's Order No")
     buyer_order_date = fields.Date(string="Buyer's Order Date")
     proforma_invoice_date = fields.Date(string="Proforma Invoice Date")
