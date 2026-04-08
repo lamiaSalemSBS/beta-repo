91ae7ab7ecc8f52586588ba1c9a2ca52ba439428
Mohamed Hamed <mohamed.hamed@gmail.com>
2026-04-08 11:15:58 +0200

    [IMP]validition and quality check

--- addons/sbs_stock_custom/__manifest__.py
+++ addons/sbs_stock_custom/__manifest__.py
@@ -2,10 +2,10 @@
     "name": "Stock Custom",
     "version": "1.0",
     "category": "Stock",
-    "author": "Mohamed HAmed",
+    "author": "Mohamed Hamed",
     "depends": ["base", "stock"],
     "data": [
-        "views/stock_move_view.xml",
+        # "views/stock_move_view.xml",
     ],
     "installable": True,
     "application": False,
